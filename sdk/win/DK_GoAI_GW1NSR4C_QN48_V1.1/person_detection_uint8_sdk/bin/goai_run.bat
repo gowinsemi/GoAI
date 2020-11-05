b5120b7ff33c2f0d6e46dc2399f02024b0e04307
gowinsemi <47979992+gowinsemi@users.noreply.github.com>
2020-11-05 10:09:16 -0600

    Updated uint8 sdk based on build revision 360

--- sdk/win/DK_GoAI_GW1NSR4C_QN48_V1.1/person_detection_uint8_sdk/bin/goai_run.bat
+++ sdk/win/DK_GoAI_GW1NSR4C_QN48_V1.1/person_detection_uint8_sdk/bin/goai_run.bat
@@ -8,8 +8,8 @@ echo ---------------------------------------------------------------------------
 echo.
 
 
-set tflite_model_file=C:\projects\AI\GoAI_sdk\sdk\win\DK_GoAI_GW1NSR4C_QN48_V1.1\person_detection_uint8_sdk\data\tf_model\person_detection.tflite
-set gowin_programmer_path=C:\Gowin\Gowin_V1.9.7Beta\Programmer
+set tflite_model_file=D:\user-bak\Users\root\Desktop\person_detection_uint8_sdk\data\tf_model\person_detection.tflite
+set gowin_programmer_path=D:\user-bak\Users\root\Desktop\person_detection_uint8_sdk\plugins\programmer
 set keil_path=C:/Keil_v5
 
 echo.
