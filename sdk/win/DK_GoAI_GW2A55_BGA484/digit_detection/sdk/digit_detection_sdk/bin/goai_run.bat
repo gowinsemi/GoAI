b2aad7a471630145a5c0bb692cad4fc7533d95a2
gowinsemi <47979992+gowinsemi@users.noreply.github.com>
2021-03-29 16:46:48 -0500

    a55 sdk updates/additions

---
+++ sdk/win/DK_GoAI_GW2A55_BGA484/digit_detection/sdk/digit_detection_sdk/bin/goai_run.bat
@@ -0,0 +1,18 @@
+@echo off
+
+echo.
+echo ----------------------------------------------------------------------------------
+echo          COPYRIGHT (C) 2014-2021 GOWIN SEMICONDUCTOR TECHNOLOGY CO.,LTD.
+echo                             ALL RIGHTS RESERVED.
+echo ----------------------------------------------------------------------------------
+echo.
+
+echo.
+echo GOAI DIGIT_DETECTION BEGINNING...
+
+call digit_detection.exe --device GW2A55C --tflite_model_file C:\Users\liukai\Desktop\digit_detection_sdk\data\tf_model\digit_detection.tflite --gowin_eda_path D:\Gowin\Gowin_V1.9.7.02Beta --keil_path D:\Keil_v5 --training_type keras
+
+echo.
+echo GOAI DIGIT_DETECTION BUILD DONE!
+
+pause
